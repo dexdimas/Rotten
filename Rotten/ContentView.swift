4c076a9641b1533f5eb4db679a176edf74a61679
Andimas Bagaswara <andimas.bagaswara@gmail.com>
2020-10-01 19:15:19 +0800

    Initial Commit

---
+++ Rotten/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rotten
+//
+//  Created by Andimas Bagaswara on 01/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rotten/RottenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RottenApp.swift
+//  Rotten
+//
+//  Created by Andimas Bagaswara on 01/10/20.
+//
+
+import SwiftUI
+
+@main
+struct RottenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
