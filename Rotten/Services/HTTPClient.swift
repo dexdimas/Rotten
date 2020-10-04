17846d1b952da5c30a3a81741932d4624bd56431
Andimas Bagaswara <andimas.bagaswara@gmail.com>
2020-10-04 08:28:18 +0800

    Add MovieDetail Model and HTTPClient End Point for MovieDetail

--- Rotten/Services/HTTPClient.swift
+++ Rotten/Services/HTTPClient.swift
@@ -15,21 +15,30 @@ enum NetworkError: Error {
 
 class HTTPClient: ObservableObject {
     
-    func getMoviesBy(search: String, completion: @escaping (Result<[Movie]?,NetworkError>) -> Void) {
+    func getMovieDetailsBy(imdbId: String, completion: @escaping (Result<MovieDetail, NetworkError>) -> Void
+    ) {
         
-        guard let url = URL.forMoviesByName(search) else {
-            return completion(.failure(.badURL))
-        }
+        guard let url = URL.forMoviesByImdbId(imdbId) else { return completion(.failure(.badURL)) }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             
-            guard let data = data, error == nil else {
-                return completion(.failure(.noData))
-            }
+            guard let data = data, error == nil else { return completion(.failure(.noData)) }
             
-            guard let moviesResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else {
-                return completion(.failure(.decodingError))
-            }
+            guard let movieDetail = try? JSONDecoder().decode(MovieDetail.self, from: data) else { return completion(.failure(.decodingError)) }
+            
+            completion(.success(movieDetail))
+        }
+    }
+    
+    func getMoviesBy(search: String, completion: @escaping (Result<[Movie]?,NetworkError>) -> Void) {
+        
+        guard let url = URL.forMoviesByName(search) else { return completion(.failure(.badURL)) }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else { return completion(.failure(.noData)) }
+            
+            guard let moviesResponse = try? JSONDecoder().decode(MovieResponse.self, from: data) else { return completion(.failure(.decodingError)) }
             
             completion(.success(moviesResponse.movies))
             
